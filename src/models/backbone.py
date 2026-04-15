68f16617860cc5258363af46d0d6bc2364fabceb
jchang431 <jychang@gatech.edu>
2026-04-15 01:57:49 -0400

    Update and rename backbone.py to supervised.py

--- src/models/backbone.py
+++
@@ -1,9 +0,0 @@
-import torch.nn as nn
-from torchvision.models import resnet18
-
-def get_model(num_classes=10):
-    model = resnet18(weights=None)
-    model.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
-    model.maxpool = nn.Identity()
-    model.fc = nn.Linear(512, num_classes)
-    return model
